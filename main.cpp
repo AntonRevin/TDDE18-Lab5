06cd2835771ee3fbf8cb82041d1baf0a66f5913c
Anton Revin <anton@revin.io>
2018-12-10 10:22:44 +0000

    Completed the main file.

--- main.cpp
+++ main.cpp
@@ -6,14 +6,14 @@
 
 using namespace std;
 
-void showError(string const description) {
+void printError(string const description) {
     cerr << "\033[1;31m" << description << "\033[0m" << endl;
     cerr << "Usage: ./a.out FILE [-a] [-f] [-o N]" << endl;
 }
 
 int main(int argc, char** argv) {
     if (argc < 2) {
-        showError("ERROR: No arguments given.");
+        printError("ERROR: No arguments given.");
         return -1;
     }
 
@@ -30,12 +30,16 @@ int main(int argc, char** argv) {
 
         if ((outputFormat == "-o") && (argc = 4)) {
             charLimit = stoi(argv[3]);
+            if (charLimit < 4) {
+                printError("ERROR: Words are defined as containing 3 or more characters.");
+                return -1;
+            }
         } else {
-            showError("ERROR: Second argument missing or invalid.");
+            printError("ERROR: Second argument missing or invalid.");
             return -1;
         }
     } catch (const std::exception& e) {
-        showError("ERROR: Incorrect argument list.");
+        printError("ERROR: Incorrect argument list.");
         return -1;
     }
 
@@ -45,7 +49,7 @@ int main(int argc, char** argv) {
     ifstream fileStream{};
     fileStream.open(fileName);
     if (!fileStream.is_open()) {
-        showError("ERROR: Could not open the file.");
+        printError("ERROR: Could not open the file.");
         return -1;
     }
 
@@ -66,26 +70,38 @@ int main(int argc, char** argv) {
     switch (outputFormat[1]) {
         case 'a':
             auto comparator = [](pair<string, int> left, pair<string, int> right) {
-                return left.second >= right.second;
+                return left.first.compare(right.first) < 0;
             };
             auto printer = [maxWidth](pair<string, int> currentPair) {
                 cout << left << setw(maxWidth) << currentPair.first << " " << currentPair.second << endl;
             };
             cleaner.sort(comparator);
-            cleaner.print(printer);
+            cleaner.dynamicPrint(printer);
             break;
 
         case 'f':
-
+            auto comparator = [](pair<string, int> left, pair<string, int> right) {
+                return left.second >= right.second;
+            };
+            auto printer = [maxWidth](pair<string, int> currentPair) {
+                cout << right << setw(maxWidth) << currentPair.first << " " << currentPair.second << endl;
+            };
+            cleaner.sort(comparator);
+            cleaner.dynamicPrint(printer);
             break;
 
         case 'o':
-
+            cleaner.printOrdered(charLimit);
             break;
 
         default:
-            showError("ERROR: Incorrect argument list.");
+            printError("ERROR: Incorrect argument list.");
             return -1;
             break;
     }
+
+    /*
+        Alles gut 
+    */
+    return 1;
 }
